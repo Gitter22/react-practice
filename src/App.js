c2ab48305a6a0873b83618b143ef41f72b0124dd
Pratap-Agile <pratapagile009@gmail.com>
2022-05-10 17:21:58 +0530

    successfull get data from git api and fetch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { Layout, } from 'antd';
-import { Routes, Route, } from "react-router-dom";
+import { Layout } from "antd";
+import { Routes, Route } from "react-router-dom";
 
-import AppLayout from './components/Layout'
-import ProjectList from './pages/ProjectList';
-import ServerSidePagination from './pages/Pagination/ServerSidePagination';
-import ClientSidePagination from './pages/Pagination/ClientSidePagination';
+import AppLayout from "./components/Layout";
+import ProjectList from "./pages/ProjectList";
+import ServerSidePagination from "./pages/Pagination/ServerSidePagination";
+import ClientSidePagination from "./pages/Pagination/ClientSidePagination";
 
 const { Header, Footer, Content } = Layout;
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<AppLayout />}>
-      <Route index element={<ProjectList />} />
-      <Route path="server-side-pagination" element={<ServerSidePagination />} />
-      <Route path="client-side-pagination" element={<ClientSidePagination />} />
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<ProjectList />} />
+        <Route
+          path="server-side-pagination"
+          element={<ServerSidePagination />}
+        />
+        <Route
+          path="client-side-pagination"
+          element={<ClientSidePagination />}
+        />
 
-      <Route path="*" element={<div> Hello </div>} />
-    </Route>
-  </Routes >
+        <Route path="*" element={<div> Hello </div>} />
+      </Route>
+    </Routes>
+  );
 }
 export default App;
--- src/pages/Pagination/ClientSidePagination.js
+++ src/pages/Pagination/ClientSidePagination.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { Layout, Card } from "antd";
+
+const { Meta } = Card;
 
 const ClientSidePagination = () => {
   const [posts, setPosts] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
+  const [postNumber] = useState(5);
   console.log("dataa", posts);
+
+  const currentPageNumber = pageNumber * postNumber - postNumber;
+  const paginatedPosts = posts.splice(currentPageNumber, postNumber);
 
   const handlePrev = () => {
     if (pageNumber === 1) return;
@@ -13,24 +20,43 @@ const ClientSidePagination = () => {
     setPageNumber(pageNumber + 1);
   };
 
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "application/vnd.github.v3+json");
+  myHeaders.append(
+    "Authorization",
+    "token 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"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
   useEffect(() => {
-    fetch("https://www.postman.com/collections/dca7d0ffbb60801a7f28")
+    fetch("https://api.github.com/users?per_page=100", requestOptions)
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
-      });
-  });
+      })
+      .catch((err) => console.log("error", err));
+  }, []);
 
   return (
-    <div className="container mt-t5">
+    <div className="site-card-wrapper">
       <h2 className="text-primary mb-3">ClientSidePagination</h2>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <p>{post.name} </p>
-        </div>
+      {paginatedPosts.map((post) => (
+        <Card
+          key={post.id}
+          hoverable
+          style={{ width: 240, marginTop: "30px" }}
+          // cover={<img alt="example" src={post.avatar_url} />}
+        >
+          <h2> {post.login} </h2>
+        </Card>
       ))}
       <div>Page {pageNumber} </div>
-      <div>
+      <div style={{ marginBottom: "100px" }}>
         <button onClick={handlePrev}>prev</button>
         <button onClick={handleNext}>next</button>
       </div>
